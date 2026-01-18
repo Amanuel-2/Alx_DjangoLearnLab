8527a5201548f38c430a0d33c75dd24e2b7a53fe
Amanuel <amanuelwendimu19@gmail.com>
2026-01-18 23:00:39 +0300

    Fix: commit-4

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views  # import the whole views module
+
+
+urlpatterns = [
+    # Books and Libraries
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication
+    path('register/', views.register_view, name='register'),  # now uses views.register_view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+]
--- django-models/django_models/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI config for django_models project.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
-
-application = get_wsgi_application()
