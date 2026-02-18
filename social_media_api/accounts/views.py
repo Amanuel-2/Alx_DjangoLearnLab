63a274a240144fe8eecebf973742d53fc6a61c4f
Amanuel <amanuelwendimu19@gmail.com>
2026-02-18 16:06:07 +0300

    fix Project Setup and User.

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,9 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
-from .models import User
-from .serializers import RegisterSerializer, ProfileSerializer
+from django.contrib.auth import authenticate, get_user_model
+from .serializers import RegisterSerializer, LoginSerializer
+
+User = get_user_model()
 
 
 class RegisterView(generics.CreateAPIView):
@@ -11,14 +13,43 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        Token.objects.create(user=user)
+
+        token = Token.objects.get(user=user)
+
+        return Response({
+            "user": user.username,
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        token = serializer.validated_data["token"]
+
+        return Response({
+            "user": user.username,
+            "token": token
+        })
 
 
 class ProfileView(generics.RetrieveAPIView):
-    serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_object(self):
-        return self.request.user
+    def get(self, request):
+        return Response({
+            "id": request.user.id,
+            "username": request.user.username,
+            "email": request.user.email,
+            "bio": request.user.bio,
+        })
