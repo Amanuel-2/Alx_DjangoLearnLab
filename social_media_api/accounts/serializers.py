9634c6fe3edc5264ddd7c9430b709b2b78e149b7
Amanuel <amanuelwendimu19@gmail.com>
2026-02-18 15:55:43 +0300

    Project Setup and User Authentication for a Social Media API.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -18,50 +18,10 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password'],
             bio=validated_data.get('bio', '')
         )
-
-        Token.objects.create(user=user)
         return user
 
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-
-    def validate(self, data):
-        user = authenticate(
-            username=data['username'],
-            password=data['password']
-        )
-
-        if not user:
-            raise serializers.ValidationError("Invalid credentials")
-
-        token, created = Token.objects.get_or_create(user=user)
-
-        return {
-            'user': user,
-            'token': token.key
-        }
-
-
 class ProfileSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    following_count = serializers.SerializerMethodField()
-
     class Meta:
         model = User
-        fields = [
-            'id',
-            'username',
-            'email',
-            'bio',
-            'profile_picture',
-            'followers_count',
-            'following_count'
-        ]
-
-    def get_followers_count(self, obj):
-        return obj.followers.count()
-
-    def get_following_count(self, obj):
-        return obj.following.count()
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from .models import User
-from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
+from .serializers import RegisterSerializer, ProfileSerializer
 
 
 class RegisterView(generics.CreateAPIView):
@@ -11,30 +11,9 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
         user = serializer.save()
-        token = Token.objects.get(user=user)
-
-        return Response({
-            "user": ProfileSerializer(user).data,
-            "token": token.key
-        }, status=status.HTTP_201_CREATED)
-
-
-class LoginView(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        return Response({
-            "user": ProfileSerializer(serializer.validated_data['user']).data,
-            "token": serializer.validated_data['token']
-        })
+        Token.objects.create(user=user)
 
 
 class ProfileView(generics.RetrieveAPIView):
