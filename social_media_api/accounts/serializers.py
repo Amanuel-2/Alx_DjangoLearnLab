e2f8bbe06af03db1068abf22af82f68c584daa7e
Amanuel <amanuelwendimu19@gmail.com>
2026-02-18 16:04:10 +0300

    fix Project Setup and User

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
@@ -12,16 +13,36 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            bio=validated_data.get('bio', ''),
         )
+
+        Token.objects.create(user=user)
+
         return user
 
 
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        from django.contrib.auth import authenticate
+
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return {
+            "user": user,
+            "token": token.key
+        }
